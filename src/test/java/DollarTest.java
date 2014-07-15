8d47067a6c1c1a6f0e927e1106d6ac545f965345
Lukasz Gajowy <lukasz.gajowy@gmail.com>
2014-07-15 13:54:41 +0200

    Degenerate objects

--- src/test/java/DollarTest.java
+++ src/test/java/DollarTest.java
@@ -13,4 +13,13 @@ public class DollarTest {
         five.times(2);
         assertEquals(10, five.amount);
     }
+
+    @Test
+    public void shouldMultiplyWithoutChangingTheDollar() throws Exception {
+        Dollar five = new Dollar(5);
+        Dollar product = five.times(2);
+        assertEquals(10, product.amount);
+        product = five.times(3);
+        assertEquals(15, product.amount);
+    }
 }
