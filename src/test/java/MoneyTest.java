f3e4457f510a3c6b4a0521389eb5fe8392c5c3e3
Lukasz Gajowy <lukasz.gajowy@gmail.com>
2014-07-16 17:06:04 +0200

    cleanup

--- src/test/java/MoneyTest.java
+++ src/test/java/MoneyTest.java
@@ -55,33 +55,5 @@ public class MoneyTest {
         assertEquals(five, sum.augend);
         assertEquals(five, sum.addend);
     }
-
-    @Test
-    public void shouldReduceSum() throws Exception {
-        Expression sum = new Sum (Money.dollar(3), Money.dollar(4));
-        Bank bank = new Bank();
-        Money result = bank.reduce(sum, "USD");
-        assertEquals(Money.dollar(7), result);
-    }
-
-    @Test
-    public void shouldReduceMoney() throws Exception {
-        Bank bank = new Bank();
-        Money result = bank.reduce(Money.dollar(1), "USD");
-        assertEquals(Money.dollar(1), result);
-    }
-
-    @Test
-    public void shouldReduceMoneyDifferentCurrency() throws Exception {
-        Bank bank = new Bank();
-        bank.addRate("CHF", "USD", 2);
-        Money result = bank.reduce(Money.franc(2), "USD");
-        assertEquals(Money.dollar(1), result);
-    }
-
-    @Test
-    public void shouldRateIndenticalBe1() throws Exception {
-        assertEquals(1, new Bank().rate("USD", "USD"));
-    }
 }
 
